6b1035b2bca6e4289fa091a405d1da6359d2a51e
Luke Thompson <endeavour9@gmail.com>
2024-04-28 16:35:09 +1000

    publish the docker image manually to ecr, then use serverless to reference the image

--- build.sh
+++ build.sh
@@ -14,3 +14,14 @@ rm -rf ./temp-build
 
 cd docker
 docker build -t java-opencv-lambda .
+
+if [ -z "$DOCKER_TAG" ]; then
+  echo "DOCKER_TAG is not set. Exiting..."
+  exit 1
+fi
+
+aws ecr --profile personal get-login-password --region ap-southeast-2  | docker login --username AWS --password-stdin 204244381428.dkr.ecr.ap-southeast-2.amazonaws.com
+REPOSITORY_TAG="204244381428.dkr.ecr.ap-southeast-2.amazonaws.com/java-opencv-lambda:${DOCKER_TAG}"
+REPOSITORY_TAG_LATEST="204244381428.dkr.ecr.ap-southeast-2.amazonaws.com/java-opencv-lambda:latest"
+echo "$(date): Building and pushing ${REPOSITORY_TAG}..."
+docker buildx build --push -t "${REPOSITORY_TAG}" -t "${REPOSITORY_TAG_LATEST}" --platform linux/arm64 .
