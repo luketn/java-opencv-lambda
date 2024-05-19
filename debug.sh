e30bea31002184994fd63ddf4efb1772905b7ba2
Luke Thompson <endeavour9@gmail.com>
2024-05-19 21:36:05 +1000

    kill the previous container run

--- debug.sh
+++ debug.sh
@@ -1,4 +1,8 @@
+echo "Running the Docker image with RIE for local lambda API Gateway debugging..."
+docker kill java-opencv-lambda || true
+docker rm java-opencv-lambda || true
 
+# ref: https://github.com/aws/aws-lambda-runtime-interface-emulator?tab=readme-ov-file#installing
 docker run \
     --name java-opencv-lambda \
     -it \
